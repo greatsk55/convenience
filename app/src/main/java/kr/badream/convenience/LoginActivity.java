d28f0c770cbfc984243a6632b3ccf4e85b57eabd
greatsk55 <greatsk553@naver.com>
2016-06-10 14:15:55 +0900

    account kit

--- app/src/main/java/kr/badream/convenience/LoginActivity.java
+++ app/src/main/java/kr/badream/convenience/LoginActivity.java
@@ -5,12 +5,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.accountkit.AccessToken;
 import com.facebook.accountkit.AccountKit;
+import com.facebook.accountkit.AccountKitLoginResult;
+import com.facebook.accountkit.ui.AccountKitActivity;
+import com.facebook.accountkit.ui.AccountKitConfiguration;
+import com.facebook.accountkit.ui.LoginType;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
@@ -21,6 +28,9 @@ import java.util.Arrays;
  */
 public class LoginActivity extends Activity {
 
+
+    public static int APP_REQUEST_CODE = 99;
+
     //페이스북 로그인, 콜백
     private LoginButton fbButton;
     private CallbackManager callbackManager;
@@ -29,13 +39,14 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //facebook 초기화
         FacebookSdk.sdkInitialize(getApplicationContext());
         callbackManager = CallbackManager.Factory.create();
 
-        setContentView(R.layout.activity_login);
-
         //accountKit 초기화
         AccountKit.initialize(getApplicationContext());
+
+        setContentView(R.layout.activity_login);
 
         fbButton = (LoginButton) findViewById(R.id.login_button);
         fbButton.setReadPermissions(Arrays.asList("public_profile", "user_friends", "email"));
@@ -58,6 +69,28 @@ public class LoginActivity extends Activity {
                 Log.i("bhc :", "Login attempt failed.");
             }
         });
+
+
+        //현재 로그인 돼었는지 확인한다.
+        AccessToken accessToken = AccountKit.getCurrentAccessToken();
+        if (accessToken != null) {
+            //Handle Returning User
+        } else {
+            //Handle new or logged out user
+        }
+    }
+
+    public void onLoginPhone(final View view) {
+        final Intent intent = new Intent( LoginActivity.this, AccountKitActivity.class);
+        AccountKitConfiguration.AccountKitConfigurationBuilder configurationBuilder =
+                new AccountKitConfiguration.AccountKitConfigurationBuilder(
+                        LoginType.PHONE,
+                        AccountKitActivity.ResponseType.CODE); // or .ResponseType.TOKEN
+        // ... perform additional configuration ...
+        intent.putExtra(
+                AccountKitActivity.ACCOUNT_KIT_ACTIVITY_CONFIGURATION,
+                configurationBuilder.build());
+        startActivityForResult(intent, APP_REQUEST_CODE);
     }
 
 
@@ -67,5 +100,38 @@ public class LoginActivity extends Activity {
         callbackManager.onActivityResult(requestCode, resultCode, data);
 
         super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == APP_REQUEST_CODE) { // confirm that this response matches your request
+            AccountKitLoginResult loginResult = data.getParcelableExtra(AccountKitLoginResult.RESULT_KEY);
+            String toastMessage;
+            if (loginResult.getError() != null) {
+                toastMessage = loginResult.getError().getErrorType().getMessage();
+                //TODO 로그인 실패 액티비티
+            } else if (loginResult.wasCancelled()) {
+                toastMessage = "Login Cancelled";
+            } else {
+                if (loginResult.getAccessToken() != null) {
+                    toastMessage = "Success:" + loginResult.getAccessToken().getAccountId();
+                } else {
+                    toastMessage = String.format(
+                            "Success:%s...",
+                            loginResult.getAuthorizationCode().substring(0,10));
+                }
+
+                // If you have an authorization code, retrieve it from
+                // loginResult.getAuthorizationCode()
+                // and pass it to your server and exchange it for an access token.
+
+                // Success! Start your next activity...
+                //TODO Login 후 처리
+            }
+
+            // Surface the result to your user in an appropriate way.
+            Toast.makeText(
+                    this,
+                    toastMessage,
+                    Toast.LENGTH_LONG)
+                    .show();
+        }
     }
 }
