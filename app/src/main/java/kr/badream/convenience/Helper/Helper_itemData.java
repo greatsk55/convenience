d1cb58920a72aec78938f18820a68a82e15f6cbf
greatsk55 <greatsk553@naver.com>
2016-06-12 18:38:12 +0900

    workin to get item list

--- app/src/main/java/kr/badream/convenience/Helper/Helper_itemData.java
+++ app/src/main/java/kr/badream/convenience/Helper/Helper_itemData.java
@@ -12,39 +12,25 @@ import kr.badream.convenience.R;
 //TODO 방정보를 저장할 자료형 클래스
 public class Helper_itemData {
 
-    private int id;
-    private int conv_id;
-    private String itemname;
-    private int price;
-    private int image;
+    public int prodID;
+    public int storeID;
+    public int mainCategory;
+    public int subCategory;
 
-
-    public ArrayList<Integer> list;
+    public String url;
+    public String name;
+    public int price;
 
     public Helper_itemData(){
-        //list = new ArrayList<Integer>();
-       // list.add(0, R.drawable.facebook_gray);
-
-        itemname = "삼각김밥";
-        image = R.drawable.cu;
-        price = 700;
     }
 
-    public void setItem(int id, int conv_id, String itemname, int price, int image){
-        this.id = id;
-        this.conv_id = conv_id;
-        this.itemname = itemname;
+    public Helper_itemData(int prodID, int storeID, int mainCategory, int subCategory, String url, String name, int price){
+        this.prodID = prodID;
+        this.storeID = storeID;
+        this.mainCategory = mainCategory;
+        this.subCategory = 0;
+        this.name = name;
         this.price = price;
-        this.image = image;
-    }
-
-    public int getPrice(){
-        return price;
-    }
-    public String getName(){
-        return itemname;
-    }
-    public int getImage(){
-        return image;
+        this.url = url;
     }
 }
