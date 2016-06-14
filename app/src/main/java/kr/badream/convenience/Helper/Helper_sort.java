f4c66e49252da14be3988efc28852c1dd56761a0
greatsk55 <greatsk553@naver.com>
2016-06-15 00:26:51 +0900

    get Likes and reviews

--- app/src/main/java/kr/badream/convenience/Helper/Helper_sort.java
+++ app/src/main/java/kr/badream/convenience/Helper/Helper_sort.java
@@ -3,30 +3,28 @@ package kr.badream.convenience.Helper;
 import java.text.Collator;
 import java.util.Comparator;
 
-import kr.badream.convenience.Adapter.Item_list_view;
-
 /**
  * Created by Administrator on 2016-06-14.
  */
 public class Helper_sort {
 
     //Comparator 를 만든다.
-    public final static Comparator<Item_list_view> comparator_Item_list_view_like = new Comparator<Item_list_view>() {
+    public final static Comparator<Helper_itemInfo> comparator_Item_list_view_like = new Comparator<Helper_itemInfo>() {
         @Override
-        public int compare(Item_list_view item1, Item_list_view item2) {
+        public int compare(Helper_itemInfo item1, Helper_itemInfo item2) {
 //            return Collator.getInstance().compare(item1.getItem_like_number(),item2.getItem_like_number());
-            return ( item1.getItem_like_number() > item2.getItem_like_number() ? 1:-1);
+            return ( item1.likes > item2.likes ? 1:-1);
         }
 
         private final Collator collator = Collator.getInstance();
 
     };
 
-    public final static Comparator<Item_list_view> comparator_Item_list_view_review = new Comparator<Item_list_view>() {
+    public final static Comparator<Helper_itemInfo> comparator_Item_list_view_review = new Comparator<Helper_itemInfo>() {
         @Override
-        public int compare(Item_list_view item1, Item_list_view item2) {
+        public int compare(Helper_itemInfo item1, Helper_itemInfo item2) {
 //            return Collator.getInstance().compare(item1.getItem_like_number(),item2.getItem_like_number());
-            return ( item1.getItem_review_number() > item2.getItem_review_number() ? 1:-1);
+            return ( item1.reviews > item2.reviews ? 1:-1);
         }
 
         private final Collator collator = Collator.getInstance();
