488c5e649323572631eff746a3f80e04a4d270d9
greatsk55 <greatsk553@naver.com>
2016-06-15 13:41:54 +0900

    get Likes and reviews and views

--- app/src/main/java/kr/badream/convenience/Helper/ApiInterface.java
+++ app/src/main/java/kr/badream/convenience/Helper/ApiInterface.java
@@ -23,7 +23,7 @@ public interface ApiInterface {
     @POST("index.php/{ID}/{flag}/{name}/{gender}")
     Call<User> registration(@Path("ID") String id, @Path("flag") int flag, @Path("name") String name, @Path("gender") int gender);
 
-    @POST("index.php/{userID}/{userName}/{prodID}/{price}/{contents}")
+    @GET("index.php/{userID}/{userName}/{prodID}/{price}/{contents}")
     Call<Helper_reviewData> postReview(@Path("userID") int userID, @Path("userName") String userName, @Path("prodID") String prodID, @Path("price") String price,
                                        @Path("contents") String contents);
 
